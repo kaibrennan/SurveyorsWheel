17908e2c329bb23435cef5177adf1dabc93c8de0
stimpy-42 <kai.j.brennan@gmail.com>
2025-04-13 11:41:03 -0600

    Fully Functional, Absolute Value Surveyors Wheel

--- surveyors_wheel/src/main.c
+++ surveyors_wheel/src/main.c
@@ -7,14 +7,19 @@ int main(void) { // use the 3.3v as vcc instead of the 5v.
 	UARTsetup(9600); //uart setup from 251 labs
 	GPIOF_setup();
 	UARTstring("Surveyor's Wheel Initialized\n");
+    UARTstring("Dist. traveled: 0.00 in\n");
 	encoder_setup();// method defined in util.c
 
 	int count = 0; //counter for clicks of rotation; later this will be multiplied by the degree value and radius to get distance.
+    const float circumference = 15.375; // inches per full rotation
+	const float dist_per_click = circumference / 24.0f; //resolution of the sensor (i.e. clicks per 360 deg rotation)
+	float distance = 0.0f; //dist tracker in inches.
 
     while (1) { //this while loop has the main body of the counter, each time pin B on the encoder goes low (the sensor is turned clockwise) it increments.
         if ((gpio_port_read(GPIOB) & GPIO2) == 0) {
             count++;
-            UARTprintf("Count: %d\n", count);
+            distance += dist_per_click;
+            UARTprintf("Dist. traveled: %.2f in\n", distance);
             gpio_set(GPIOF, LED_B);
             while ((gpio_port_read(GPIOB) & GPIO2) == 0); // wait for release
         }
